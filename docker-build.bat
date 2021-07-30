1455a02f17b5f99135d978c9d92510061679a180
rectangulared <richardfoxwarframe@gmail.com>
2021-07-30 09:44:37 +0300

    Fully removed maven docker build plugin

--- docker-build.bat
+++ docker-build.bat
@@ -1,3 +1,5 @@
-docker build -t pizza-builder/pizza-builder ./pizza-builder
+docker build -t api-gateway ./api-gateway
+docker build -t naming-server ./naming-server
+docker build -t pizza-builder ./pizza-builder
 docker build -t pizza-cart ./pizza-cart
 docker build -t pizza-viewer ./pizza-viewer
